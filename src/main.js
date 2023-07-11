d9bf1b5c60806ae8c0436e17d8e95b30fc83b4b4
Vincenzo <vincenzo.villani@studio.unibo.it>
2023-07-11 12:33:24 +0200

    public-release-v0.14.6
    - Global ComponentsRegistry auto import with glob
    - Lite Routing example

--- src/main.js
+++ src/main.js
@@ -1,14 +1,41 @@
 // import Clean.js
 import {LE_InitWebApp, RenderApp} from 'cle.js/lib'
 
+import "./utils/csz.js"
+
+import "./app/global-di-components/**/*.js" // auto import all global di components with glob and wildcard
+
 import { Homepage } from './app/pages/home.page.deps'
-
-import "./app/global-di-components"
-
-import csz from 'csz'
-
-// fix csz to not use template literal
-window.css = (v)=>csz([v]) 
 
 // render app
 LE_InitWebApp(async ()=>{ RenderApp(document.body, await Homepage() ) })
+
+
+// or render app with lite cle routing!
+
+// import {InitRouter, Router, RedirectTo} from "cle.js/routing/lite_routing"
+// 
+// export const HomePageRoute = async (state, params)=>{
+// 
+//   console.log(state, params)
+// 
+//   return await Homepage()
+// 
+// }
+// 
+// LE_InitWebApp(async ()=>{
+// 
+//   await InitRouter({
+// 
+//     pages: {
+//       "/": HomePageRoute,
+// 
+//       "/redirect": RedirectTo("/"),
+//       // "/details/:detail_id": DetailPageRoute, // parametric
+// 
+//     },
+//   
+//     defaultRoute: "/"
+//   })
+// 
+// })
